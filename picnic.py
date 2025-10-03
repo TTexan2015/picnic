7f1007699d3cdd16ef9a0f90a77a5a7ca8ad5dd4
TTexan2015 <enamul@outlook.com>
2025-10-03 00:17:10 -0500

    Update picnic.py

--- picnic.py
+++ picnic.py
@@ -5,7 +5,7 @@ headcount = 35
 venue_cost = 120
 lunch_cost_per_person = 15
 snacks_drinks_miscellaneous_cost_per_person = 5
-lunch_people = 40  # ordering extra meals for buffer
+lunch_people = headcount + 5  # ordering extra meals for buffer
 
 # Calculations
 total_lunch_cost = lunch_people * lunch_cost_per_person
@@ -15,4 +15,5 @@ per_person_cost = total_cost / headcount
 
 # Output
 print("Total cost:", total_cost)
-print("Per person cost:", round(per_person_cost, 2))
+print("Per person cost:", per_person_cost)
+
